2f1cd4b4d25e11e62c864beede5c4f5dfb2b6058
Beck <therealpiggylongsnout@gmail.com>
2018-10-24 09:27:11 -0700

    Finish implementing deal().

--- deal.js
+++ deal.js
@@ -1,18 +1,18 @@
 const columnCount = 7;
 
 function deal() {
-    //alert("Deal!");
     let a = 0
-for(let row = 0; row < 7; row++){
-a = 0
-for(let col = row; col < 7; col++){
-a = a + 1
-   let card = this.cards[col];
-   card.reversed = true
-   if(a == 1) {card.reversed = false} else {card.reversed = true};
 
-    this.columns[col].cards.push(card);
+    for(let row = 0; row < 7; row++){
 
-}
-}
+        for(let col = row; col < 7; col++){
+
+             let card = this.cards[a];
+             a++
+             card.reversed = false
+            if(col == row == 1) {card.reversed = false} else {card.reversed = true};
+            this.columns[col].cards.push(card);
+
+       }
+    }
 }
