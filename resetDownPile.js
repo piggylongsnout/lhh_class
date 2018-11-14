a288f046ccc35baf3106846a78257400f4ebedf8
Ian Petersen <ispeters@gmail.com>
2018-11-14 01:50:00 -0800

    Implement the draw pile.

---
+++ resetDownPile.js
@@ -0,0 +1,7 @@
+function resetDownPile(downPile, upPile) {
+    alert('You need to:\n' +
+        '  - move upPile.cards to downPile,\n' +
+        '  - reverse the order ofdownPile\'s cards,\n' +
+        '  - clear upPile,\n' +
+        '  - ensure card.reversed is true for each card');
+}
